b9ce56c3aa89336f3a8354a1b9de369d4bc8b07f
Lee Thomas <lee@ahluka.co.uk>
2019-10-20 15:24:04 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> {"Lee", "Sab", "Bing Bing"};
+
+            foreach (var name in names) {
+                Console.WriteLine($"Hello {name}!");
+            }
+        }
+    }
+}
